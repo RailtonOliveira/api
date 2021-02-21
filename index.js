f2274b7bbece5a17096aa23953f6770aa4fcd76a
Railton Oliveira <railtonoliiveira@gmail.com>
2021-02-21 14:53:07 -0400

    Add script to create table on mysql

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const customExpress = require('./config/customExpress')
 const conexao = require('./insfraestrutura/conexao')
+const Tabelas = require('./insfraestrutura/tabelas')
 
 conexao.connect(erro => {
     if (erro) {
         console.log(erro)
     } else {
         console.log('conectado com sucesso!')
+
+        Tabelas.init(conexao)
         const app = customExpress()
 
         app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
