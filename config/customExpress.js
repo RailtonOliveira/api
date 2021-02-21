2475bad5d0b83deaef301d3d4526e2d0ddee9aa5
Railton Oliveira <railtonoliiveira@gmail.com>
2021-02-21 13:17:47 -0400

    Install consign and separating responsibilities

---
+++ config/customExpress.js
@@ -0,0 +1,12 @@
+module.exports = () => {
+    const express = require('express')
+    const consign = require('consign')
+
+    const app = express()
+
+    consign()
+        .include('controllers')
+        .into(app)
+
+    return app
+}
